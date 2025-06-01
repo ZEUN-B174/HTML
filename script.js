8580c37df585ed4fb0294afc0f3a08d3b40e1a05
ZEUN <jaeyoun632@gmail.com>
2025-06-01 22:25:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let usernames = ['admin', 'user1', 'user2'];
 let passwords = ['qwer1234', 'abcd1234', '1234abcd'];
 
+let subtitles = ['개요', '종류', '모양에 따른 분류','형태', '출처'];
+let subtitlesId = ['outline', 'kind', 'class', 'shape', 'source'];
+let subtitlesIndex = ['1. ', '2. ', '3. ', '4. ', '5. '];
+
 let currentUser = null;
 let loginStatus = false;
+let darkMode = false;
 
 function register() {
     if (!loginStatus) {
-        let username = prompt('아이디를 입력해주세요:');
+        let username = prompt('아이디를 입력해주세요');
         if (!username) {
             alert('아이디를 입력하지 않았습니다.');
             return;
@@ -16,7 +21,7 @@ function register() {
             return;
         }
 
-        let password = prompt('비밀번호를 입력해주세요:');
+        let password = prompt('비밀번호를 입력해주세요');
         if (!password) {
             alert('비밀번호를 입력하지 않았습니다.');
             return;
@@ -41,12 +46,12 @@ function loginLogout() {
         document.getElementById('register').innerText = '회원가입';
     }
     else {
-        let username = prompt('아이디를 입력해주세요:');
+        let username = prompt('아이디를 입력해주세요');
         if (!username) {
             alert('아이디를 입력하지 않았습니다.');
             return;
         }
-        let password = prompt('비밀번호를 입력해주세요:');
+        let password = prompt('비밀번호를 입력해주세요');
         if (!password) {
             alert('비밀번호를 입력하지 않았습니다.');
             return;
@@ -67,6 +72,7 @@ function loginLogout() {
 
 function search() {
     const input = document.getElementById('searchInput').value.trim();
+    document.getElementById('searchInput').value = '';
 
     if (input === '') {
         alert('검색어를 입력해주세요.');
@@ -79,4 +85,41 @@ function search() {
     else {
         window.open('noresult.html', '_self');
     }
+}
+
+function edit() {
+    if (loginStatus) {
+        let subtitle = prompt('수정할 문서의 소제목을 입력해주세요');
+        if (!subtitle) {
+            alert('소제목을 입력하지 않았습니다.');
+            return;
+        }
+        else if (!subtitles.includes(subtitle)) {
+            alert('존재하지 않는 소제목입니다.');
+            return;
+        }
+
+        const index = subtitles.indexOf(subtitle);
+        
+        let subtitleId = subtitlesId[index];
+        const toToc = document.createElement('a');
+        toToc.href = '#toc';
+        toToc.innerText = subtitlesIndex[index];
+
+        alert('현재로선 문서의 소제목만 수정할 수 있습니다.');
+        let content = prompt('수정할 내용을 입력해주세요');
+        if (content) {
+            document.getElementById(subtitleId).innerText = '';
+            document.getElementById(subtitleId).appendChild(toToc);
+            document.getElementById(subtitleId).appendChild(document.createTextNode(content));
+            subtitles[index] = content;
+            alert(`소제목을 ${content}(으)로 수정했습니다.`);
+        }
+        else {
+            alert('수정할 내용을 입력하지 않았습니다.');
+        }
+    } else {
+        alert('로그인 후에 수정할 수 있습니다.');
+    }  
+    
 }
